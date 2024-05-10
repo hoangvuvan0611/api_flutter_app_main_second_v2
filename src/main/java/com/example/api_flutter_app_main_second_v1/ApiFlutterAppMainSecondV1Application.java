80b73f6b847309a968028bf1b438049e68e48585
hoangvuvan0611 <113603055+hoangvuvan0611@users.noreply.github.com>
2024-05-10 15:54:36 +0700

    fix: convert jar file to war

--- src/main/java/com/example/api_flutter_app_main_second_v1/ApiFlutterAppMainSecondV1Application.java
+++ src/main/java/com/example/api_flutter_app_main_second_v1/ApiFlutterAppMainSecondV1Application.java
@@ -2,9 +2,10 @@ package com.example.api_flutter_app_main_second_v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiFlutterAppMainSecondV1Application {
+public class ApiFlutterAppMainSecondV1Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApiFlutterAppMainSecondV1Application.class, args);
     }
